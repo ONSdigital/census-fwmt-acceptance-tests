6f24b7068ac405a94a97f3585a29246f6c73d844
stevescorfield <steve.scorfield@ons.gov.uk>
2019-06-26 13:24:12 +0100

    Moved CE tests into new test feature

--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/utils/CSVServiceUtils.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/utils/CSVServiceUtils.java
@@ -1,13 +1,18 @@
 package uk.gov.ons.census.fwmt.tests.acceptance.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import uk.gov.ons.census.fwmt.tests.acceptance.exceptions.MockInaccessibleException;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Slf4j
 @Component
@@ -16,13 +21,32 @@ public class CSVServiceUtils {
   @Value("${service.csv.url}")
   private String csvService;
 
-  public void enableCsvService() throws IOException {
-    URL url = new URL(csvService);
-    log.info("enable-csv_service:" + url.toString());
-    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-    httpURLConnection.setRequestMethod("GET");
-    if (httpURLConnection.getResponseCode() != 200) {
-      throw new MockInaccessibleException("Failed : HTTP error code : " + httpURLConnection.getResponseCode());
-    }
+  @Value("${service.csvservice.username}")
+  private String csvServiceUsername;
+
+  @Value("${service.csvservice.password}")
+  private String csvServicePassword;
+
+  public int enableCsvService() throws IOException {
+    HttpHeaders headers = createBasicAuthHeaders(csvServiceUsername, csvServicePassword);
+
+    headers.setContentType(MediaType.APPLICATION_JSON);
+
+    RestTemplate restTemplate = new RestTemplate();
+
+    HttpEntity<String> get = new HttpEntity<>(null, headers);
+    ResponseEntity<Void> response = restTemplate.exchange(csvService, HttpMethod.GET, get, Void.class);
+
+    return response.getStatusCode().value();
+  }
+
+  private HttpHeaders createBasicAuthHeaders(String username, String password) {
+    HttpHeaders headers = new HttpHeaders();
+    final String plainCreds = username + ":" + password;
+    byte[] plainCredsBytes = plainCreds.getBytes();
+    byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+    String base64Creds = new String(base64CredsBytes);
+    headers.add("Authorization", "Basic " + base64Creds);
+    return headers;
   }
 }
--- src/test/resources/acceptancetests/CensusTests.feature
+++ src/test/resources/acceptancetests/CensusTests.feature
@@ -103,10 +103,4 @@ Feature: Census Tests
     Given TM sends a "iuacRequiredByText" Census Case Outcome to the Gateway with case ID "6c9b1177-3e03-4060-b6db-f6a8456292ef"
     And the response contains the Requestor Phone Number "07123456789" from queue "Gateway.Fulfillment.Request"
 
-  Scenario: As a Gateway I can receive a CSV file for CE and can pass the content to TM
-    Given the Gateway receives a CSV CE with case ID "2f1ea0fd-18b1-4786-b1f7-3e9a79ed1a52"
-    When the Gateway sends a Create Job message to TM with case ID "2f1ea0fd-18b1-4786-b1f7-3e9a79ed1a52"
-    And TM picks up the Create Job message with case ID "2f1ea0fd-18b1-4786-b1f7-3e9a79ed1a52"
-    Then a new case with id of "2f1ea0fd-18b1-4786-b1f7-3e9a79ed1a52" is created in TM
-
 
