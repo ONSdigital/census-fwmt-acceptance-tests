947d8158b67bcaa64b39ec177db76ad480d247c6
ThomasPoot <35296806+thomaspoot@users.noreply.github.com>
2019-09-03 15:58:01 +0100

    Added basic control over timeouts.

--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/utils/QueueUtils.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/utils/QueueUtils.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
@@ -13,70 +14,90 @@ import java.net.URISyntaxException;
 @Slf4j
 @Component
 public final class QueueUtils {
-  @Value("${service.mocktm.url}")
-  private String mockTmURL;
+    @Value("${service.mocktm.url}")
+    private String mockTmURL;
 
-  private RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate = new RestTemplate();
 
-  public long getMessageCount(String qname) {
-    RestTemplate restTemplate = new RestTemplate();
-    String messageCountUrl = mockTmURL + "/queue/count/" + qname;
-    ResponseEntity<Long> messageCount = restTemplate.getForEntity(messageCountUrl, Long.class);
-    return messageCount.getBody();
-  }
-
-  public String getMessage(String qname) throws InterruptedException {
-    RestTemplate restTemplate = new RestTemplate();
-    String messageUrl = mockTmURL + "/queue/message/?qname=" + qname;
-    String message = null;
-    for (int i = 0; i < 10; i++) {
-      ResponseEntity<String> messageEntity = restTemplate.getForEntity(messageUrl, String.class);
-      message = messageEntity.getBody();
-      if (message != null) {
-        break;
-      }
-      Thread.sleep(500);
+    public long getMessageCount(String queueName) {
+        RestTemplate restTemplate = new RestTemplate();
+        String messageCountUrl = mockTmURL + "/queue/count/" + queueName;
+        ResponseEntity<Long> messageCount = restTemplate.getForEntity(messageCountUrl, Long.class);
+        return messageCount.getBody();
     }
-    return message;
-  }
 
-  public ResponseEntity<String> getMessageOffQueueWithRoutingKey(String qname, String routingKey)
-      throws InterruptedException {
-    RestTemplate restTemplate = new RestTemplate();
-    String messageUrl = mockTmURL + "/queue/messagewithrouting/?qname=" + qname + "&routingKey=" + routingKey;
-    ResponseEntity<String> messageEntity = null;
-    for (int i = 0; i < 10; i++) {
-      try {
-        messageEntity = restTemplate.getForEntity(messageUrl, String.class);
-      } catch (Exception e) {
-        break;
-      }
-      Thread.sleep(500);
+    public String getMessage(String queueName) throws InterruptedException {
+        return getMessage(queueName, 5000, 500);
     }
-    return messageEntity;
-  }
 
-  public void sendToRMFieldQueue(String message) throws URISyntaxException {
-    String exchangeName = "";
-    String routingKey = "RM.Field";
-    RestTemplate rt = new RestTemplate();
-    HttpEntity<String> httpEntity = new HttpEntity<>(message);
-    URI uri = new URI(mockTmURL + "/queue/?exchange=" + exchangeName + "&routingkey=" + routingKey);
-    rt.postForLocation(uri, httpEntity);
-  }
+    public String getMessage(String queueName, int msTimeout) throws InterruptedException {
+        return getMessage(queueName, msTimeout, 500);
+    }
 
-  public void clearQueues() throws URISyntaxException {
-    clearQueue("Field.other");
-    clearQueue("Field.refusals");
-    clearQueue("Gateway.Actions");
-    clearQueue("Gateway.ActionsDLQ");
-    clearQueue("RM.Field");
-    clearQueue("RM.FieldDLQ");
-  }
+    public String getMessage(String queueName, int msTimeout, int msInterval) throws InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        String messageUrl = mockTmURL + "/queue/message/?qname=" + queueName;
+        String message = null;
+        int iterations = (msTimeout + msInterval - 1) / msInterval; // division rounding up
+        for (int i = 0; i < iterations; i++) {
+            ResponseEntity<String> messageEntity = restTemplate.getForEntity(messageUrl, String.class);
+            message = messageEntity.getBody();
+            if (message != null) {
+                break;
+            }
+            Thread.sleep(msInterval);
+        }
+        return message;
+    }
 
-  private void clearQueue(String queueName) throws URISyntaxException {
-    URI uri = new URI(mockTmURL + "/queue/?qname=" + queueName);
-    restTemplate.delete(uri);
-  }
+    public ResponseEntity<String> getMessageOffQueueWithRoutingKey(String queueName, String routingKey)
+            throws InterruptedException {
+        return getMessageOffQueueWithRoutingKey(queueName, routingKey, 5000, 500);
+    }
+
+    public ResponseEntity<String> getMessageOffQueueWithRoutingKey(String queueName, String routingKey, int msTimeout)
+            throws InterruptedException {
+        return getMessageOffQueueWithRoutingKey(queueName, routingKey, msTimeout, 500);
+    }
+
+    public ResponseEntity<String> getMessageOffQueueWithRoutingKey(String queueName, String routingKey, int msTimeout, int msInterval)
+            throws InterruptedException {
+        RestTemplate restTemplate = new RestTemplate();
+        String messageUrl = mockTmURL + "/queue/messagewithrouting/?qname=" + queueName + "&routingKey=" + routingKey;
+        ResponseEntity<String> messageEntity = null;
+        int iterations = (msTimeout + msInterval - 1) / msInterval; // division rounding up
+        for (int i = 0; i < iterations; i++) {
+            try {
+                messageEntity = restTemplate.getForEntity(messageUrl, String.class);
+            } catch (RestClientException e) {
+                break;
+            }
+            Thread.sleep(msInterval);
+        }
+        return messageEntity;
+    }
+
+    public void sendToRMFieldQueue(String message) throws URISyntaxException {
+        String exchangeName = "";
+        String routingKey = "RM.Field";
+        RestTemplate rt = new RestTemplate();
+        HttpEntity<String> httpEntity = new HttpEntity<>(message);
+        URI uri = new URI(mockTmURL + "/queue/?exchange=" + exchangeName + "&routingkey=" + routingKey);
+        rt.postForLocation(uri, httpEntity);
+    }
+
+    public void clearQueues() throws URISyntaxException {
+        clearQueue("Field.other");
+        clearQueue("Field.refusals");
+        clearQueue("Gateway.Actions");
+        clearQueue("Gateway.ActionsDLQ");
+        clearQueue("RM.Field");
+        clearQueue("RM.FieldDLQ");
+    }
+
+    private void clearQueue(String queueName) throws URISyntaxException {
+        URI uri = new URI(mockTmURL + "/queue/?qname=" + queueName);
+        restTemplate.delete(uri);
+    }
 
 }
