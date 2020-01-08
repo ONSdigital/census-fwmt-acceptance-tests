a2c68f0dc5de885f922db4b05e6e9800d90fe3ee
jacobharrison <jacob.harrison@qa.com>
2020-01-08 15:31:36 +0000

    comment runners

--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CCSCSVServiceTestRunner.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CCSCSVServiceTestRunner.java
@@ -1,13 +1,13 @@
-package uk.gov.ons.census.fwmt.tests.acceptance.runners;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "json:build/cucumber-report.json"},
-        features = {"src/test/resources/acceptancetests/CCSCSVServiceTests.feature"},
-        glue = {"uk.gov.ons.census.fwmt.tests.acceptance.steps.ccscsvservice"})
-public class CCSCSVServiceTestRunner {
-
-}
+//package uk.gov.ons.census.fwmt.tests.acceptance.runners;
+//
+//import cucumber.api.CucumberOptions;
+//import cucumber.api.junit.Cucumber;
+//import org.junit.runner.RunWith;
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(plugin = {"pretty", "json:build/cucumber-report.json"},
+//        features = {"src/test/resources/acceptancetests/CCSCSVServiceTests.feature"},
+//        glue = {"uk.gov.ons.census.fwmt.tests.acceptance.steps.ccscsvservice"})
+//public class CCSCSVServiceTestRunner {
+//
+//}
