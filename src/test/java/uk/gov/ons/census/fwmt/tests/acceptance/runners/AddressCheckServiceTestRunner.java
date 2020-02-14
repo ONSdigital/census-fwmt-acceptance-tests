ea8b737cab18023208f29dd72582e6086d86c05b
jacobharrison <jacobharrison@outlook.com>
2020-02-14 13:06:47 +0000

    componentScan and anno configurationations

--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/AddressCheckServiceTestRunner.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/AddressCheckServiceTestRunner.java
@@ -3,10 +3,14 @@ package uk.gov.ons.census.fwmt.tests.acceptance.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "json:build/cucumber-report.json"},
     features = {"src/test/resources/acceptancetests/AddressCheckService.feature"},
     glue = {"uk.gov.ons.census.fwmt.tests.acceptance.steps.addresscheck"})
+@Configuration
+@ComponentScan({"uk.gov.census.ffa.storage.utils"})
 public class AddressCheckServiceTestRunner {
 }
--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CECSVServiceTestRunner.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CECSVServiceTestRunner.java
@@ -3,11 +3,15 @@ package uk.gov.ons.census.fwmt.tests.acceptance.runners;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "json:build/cucumber-report.json"},
         features = {"src/test/resources/acceptancetests/CECSVServiceTests.feature"},
         glue = {"uk.gov.ons.census.fwmt.tests.acceptance.steps.cecsvservice"})
+@Configuration
+@ComponentScan({"uk.gov.census.ffa.storage.utils"})
 public class CECSVServiceTestRunner {
 
 }
