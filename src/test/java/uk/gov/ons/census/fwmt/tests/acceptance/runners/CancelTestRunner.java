fe70a798d49980d8d23c35bddb3ba3ddb2752c30
Simon <sdiaz1000@gmail.com>
2020-07-03 11:44:30 +0100

    updated dependency and enable/disabled runners

--- src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CancelTestRunner.java
+++ src/test/java/uk/gov/ons/census/fwmt/tests/acceptance/runners/CancelTestRunner.java
@@ -1,11 +1,12 @@
 package uk.gov.ons.census.fwmt.tests.acceptance.runners;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import org.springframework.context.annotation.ComponentScan;
 
-//@RunWith(Cucumber.class)
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "json:build/cucumber-report.json"},
     features = {"src/test/resources/acceptancetests/Cancel.feature"},
     glue = {"uk/gov/ons/census/fwmt/tests/acceptance/steps/inbound/"})
